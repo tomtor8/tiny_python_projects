ea93205a19a3012305fa2a1259da887343ccbfbf
tomtor8 <tomas.torday@gmail.com>
2023-12-21 20:26:02 +0100

    final hello.py

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
+"""
+    Author: Tom Torday
+    Purpose: Say hello
+"""
 
-# Purpose: Say hello
-print('Hello, World!')
+import argparse
+
+
+# ---------------------------------------
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
+    return parser.parse_args()
+
+
+# ---------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    print(f"Hello, {args.name}!")
+
+
+# ---------------------------------------
+if __name__ == "__main__":
+    main()
